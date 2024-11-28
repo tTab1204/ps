8546d70f8281646acb6b09804946793da3610398
juyoungg <juyoung@innovirus.biz>
2024-11-28 09:59:07 +0900

    feat: 268. Missing Number

---
+++ Array/268. Missing Number.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function (nums) {
+  nums.sort((a, b) => a - b);
+
+  let cnt = 0;
+
+  /**
+   * 1. nums 배열을 오름차순 정렬
+   * 2. nums 배열 순회
+   * 3. 만약, nums[i]가 cnt와 같다면, cnt++
+   * 4. 만약, nums[i]가 cnt와 다르다면, cnt를 한번 더 +1 해주고, 거기서 순회 종료
+   * 5. cnt 반환
+   */
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === cnt) {
+      cnt++;
+    } else {
+      break;
+    }
+  }
+
+  return cnt;
+};
